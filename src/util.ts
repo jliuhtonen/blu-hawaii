45f51a0fc6e78426cd91966777e477c1bd437446
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2026-08-02 13:32:03 +0300

    Update code style for new Prettier

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,5 @@
 export type MaybeUnknown<A> =
-  | { type: "known"; value: A }
-  | { type: "unknown"; value: unknown }
+  { type: "known"; value: A } | { type: "unknown"; value: unknown }
 
 export const knownValue = <A>(value: A): MaybeUnknown<A> => ({
   type: "known",
